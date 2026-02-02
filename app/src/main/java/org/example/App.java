743dd9d5d514bbc8dfc108dd1f6902ffab2a8f66
khudiyeva <153829217+khudiyeva@users.noreply.github.com>
2026-02-03 01:43:26 +0400

    Update App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -9,6 +9,6 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println("Gradle build message updated by Student B.");
     }
 }
